1c1d6f6326c618773eaf45f8e1b80c2a5ac7f119
TitaniumSigny <titaniumsigny@gmail.com>
2021-09-15 02:16:26 -0400

    now it works

--- obsidianorb.js
+++ obsidianorb.js
@@ -8,6 +8,7 @@ const array1 = [
 "This month ",
 "Next month ",
 "Yesterday ",
+"Now ",
 ];
 
 // second sentence fragment array
@@ -15,7 +16,10 @@ const array2 = [
 "a good time ",
 "a bad time ",
 "a fun time ",
-"a foolish time",
+"a foolish time ",
+"a great opportunity ",
+"a fine occasion ",
+"a cursed moment ",
 ];
 
 // third sentence fragment array
@@ -26,6 +30,8 @@ const array3 = [
 "learn a new skill",
 "do the thing",
 "get in trouble",
+"drink water",
+"look behind you",
 ];
 
 function conjure() {
@@ -38,14 +44,17 @@ function conjure() {
   const static_output = "The orb has conjured a message from the cybermists:\n";
 
   // dynamic output message
-  let output = [];
-  output.push(array1[int1]);
-  output.push("is ");
-  output.push(array2[int2]);
-  output.push("to ");
-  output.push(array3[int3]);
-  output.push(".");
+  let output = '';
+  output = (array1[int1]);
+  output = output.concat("is ");
+  output = output.concat(array2[int2]);
+  output = output.concat("to ");
+  output = output.concat(array3[int3]);
+  output = output.concat(".");
 
   //assemble final output
   return(static_output + output);
 }
+
+// test
+console.log(conjure());
